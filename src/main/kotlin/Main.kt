be60e6a0fc69de835cd5a999446adb8aa6636da2
Сергей Озерный <ozernyy@cifra.app>
2022-02-26 12:19:31 +0300

    Добавил установку размера доски при генерации расстановок ферзей

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
 fun main(args: Array<String>) {
-//    generateSubsets(0, 0)
-//    val uniqueResult = deleteDublicates(resultList)
-//    for (i in uniqueResult){
-//        println(i.joinToString())
-//    }
-//    println(uniqueResult.size)
+    setBoardSize(8)
+    generateSubsets(0, 0)
+    val uniqueResult = deleteDublicates(resultList)
+    for (i in uniqueResult){
+        println(i.joinToString())
+    }
+    println(uniqueResult.size)
 
-    generatePermutations(lenght = 4, 0)
+//    generatePermutations(lenght = 4, 0)
 }
 
 
